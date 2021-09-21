04f259a64d401cfe68433d9cd33e8305497aef1d
Andrei Koval <kowal.andrew@gmail.com>
2021-09-21 22:51:47 +0200

    moar

--- macos.sh
+++ macos.sh
@@ -11,6 +11,7 @@ defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # 3-finter drag
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
 
 info "Configuring keyboard"
 # full keyboard access for all controls
@@ -21,34 +22,13 @@ defaults write com.apple.BezelServices kDim -bool true
 defaults write com.apple.BezelServices kDimTime -int 300
 # use F-keys by default
 defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-#info "Configuring locale and languages"
-defaults write NSGlobalDomain AppleLanguages -array "en_SE" "ru_SE" "sv_SE"
-defaults write NSGlobalDomain AppleLocale -string "en_SE"
-defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
-defaults write NSGlobalDomain AppleMetricUnits -bool true
-
-#info "Configuring power management" 
-# on battery
-#sudo pmset -b sleep 30
-#sudo pmset -b displaysleep 10
-#sudo pmset -b disksleep 10
-# on power adapter
-#sudo pmset -c sleep 0
-#sudo pmset -c displaysleep 10
-#sudo pmset -c disksleep 10
-#sudo pmset -c womp 1
-#defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.Accessibility KeyRepeat -int 2
+defaults write com.apple.Accessibility InitialKeyRepeat -int 15
+defaults write com.apple.Accessibility ApplePressAndHoldEnabled -bool false
 
 info "Configuring Finder" 
 chflags nohidden ~/Library
 sudo chflags nohidden /Volumes
-defaults write com.apple.finder NewWindowTargetPath "file://$HOME"
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
@@ -90,25 +70,12 @@ defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
 defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
 defaults write com.apple.commerce AutoUpdate -bool true
 
-info "Configuring Transmission" 
-defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
-defaults write org.m0k.transmission IncompleteDownloadFolder -string "$HOME/Downloads/Incomplete"
-defaults write org.m0k.transmission DownloadAsk -bool false
-defaults write org.m0k.transmission MagnetOpenAsk -bool false
-defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
-defaults write org.m0k.transmission WarningDonate -bool false
-defaults write org.m0k.transmission WarningLegal -bool false
-defaults write org.m0k.transmission CheckRemoveDownloading -bool true
-
 info "Configuring iTerm2" 
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2"
 
 info "Configuring dock"
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/Telegram.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
 
