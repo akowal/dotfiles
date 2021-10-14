07e8ab9b264f76b90272d41c1db8a60610469d65
Andrei Koval <kowal.andrew@gmail.com>
2021-10-14 11:13:27 +0200

    fish

--- homedir/config/fish/config.fish
+++ homedir/config/fish/config.fish
@@ -3,3 +3,7 @@ eval (/opt/homebrew/bin/brew shellenv)
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/andrew/google-cloud-sdk/path.fish.inc' ]; . '/Users/andrew/google-cloud-sdk/path.fish.inc'; end
+status --is-interactive; and source (jenv init -|psub)
